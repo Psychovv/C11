a0c3dca13c82db6949c48f4ea8deebabf69f8060
Psychovv <viniciusmedina63@gmail.com>
2026-07-31 13:45:05 -0300

    add:exercise 5 cap2

---
+++ cap2/exercicio5.py
@@ -0,0 +1,10 @@
+#parte 5
+numero = int(input('Digite um numero entre 1000 e 9999: '))
+unidade = numero % 10
+dezena = (numero // 10) % 10
+centena = (numero // 100) % 10
+milhar = (numero // 1000) % 10
+print('Unidade:', unidade)
+print('Dezena:', dezena)
+print('Centena:', centena)
+print('Milhar:', milhar)
