208009500506f3d57745b740e63bc92bb517d6cb
Psychovv <viniciusmedina63@gmail.com>
2026-07-31 13:45:35 -0300

    add:exercise 8 cap2

---
+++ cap2/exercicio8.py
@@ -0,0 +1,9 @@
+#parte 8
+n1 = float(input('Digite o primeiro numero: '))
+n2 = float(input('Digite o segundo numero: '))
+print(n1 + n2)
+print(n1 - n2)
+print(n1 * n2)
+print(n1 / n2)
+print(n1 % n2)
+print(n1 ** n2)
